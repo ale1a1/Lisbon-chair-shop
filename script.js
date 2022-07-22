4b87c33db4cdba44107d22df8ad26f4105afd640
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2022-07-22 11:57:52 +0100

    ...

--- script.js
+++ script.js
@@ -2,21 +2,7 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------------------------------------------
-////////////////// VARIOUS QUERY SELECTOR ///////////////////--------------------------------------
-// ------------------------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------------------------
-
-// const tabs = document.querySelectorAll('.operations__tab');
-// const tabsContainer = document.querySelector('.operations__tab-container');
-// const tabsContent = document.querySelectorAll('.operations__content');
-
-// const header = document.querySelector('.header');
-// const allSections = document.querySelectorAll('.section');
-// const imgTargets = document.querySelectorAll('img[data-src]');
-
-// ------------------------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------------------------
-////////////////// SCROLLING ///////////////////--------------------------------------
+////////////////// SMOOTH SCROLLING ///////////////////--------------------------------------
 // ------------------------------------------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------------------------------------------
 
@@ -60,6 +46,8 @@ nav.addEventListener("mouseover", handleHover.bind(0.5));
 nav.addEventListener("mouseout", handleHover.bind(1));
 
 // Sticky navigation with the intersection observer API
+const header = document.querySelector(".header");
+console.log(header);
 
 const navHeight = nav.getBoundingClientRect().height;
 console.log(navHeight);
@@ -78,92 +66,11 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 
 headerObserver.observe(header);
 
-// Reveal sections with the intersection observer API
-// const revealSection = function (entries, observer) {
-//   const [entry] = entries;
-//   console.log(entry);
-//   if (!entry.isIntersecting) return;
-//   entry.target.classList.remove('.section--hidden');
-//   observer.unobserve(entry.target);
-// };
-// const sectionObserver = new IntersectionObserver(revealSection, {
-//   root: null,
-//   threshold: 0.15,
-// });
-// allSections.forEach(function (section) {
-//   sectionObserver.observe(section);
-//   section.classList.add('.section--hidden');
-//   console.log(section);
-// });
-
 // Make mobile navigation work
-// const btnNavEl = document.querySelector('.btn-mobile-nav');
-// btnNavEl.addEventListener('click', function () {
-//   nav.classList.toggle('nav-open');
-// });
-
-// ------------------------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------------------------
-////////////////// LAZY LOADING IMAGES ///////////////////--------------------------------------
-// ------------------------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------------------------
-
-// const loadImg = function (entries, observer) {
-//   const [entry] = entries;
-
-//   if (!entry.isIntersecting) return;
-
-// Replace src with data-src
-//   entry.target.src = entry.target.dataset.src;
-
-//   entry.target.addEventListener('load', function () {
-//     entry.target.classList.remove('lazy-img');
-//   });
-
-//   observer.unobserve(entry.target);
-// };
-
-// const imgObserver = new IntersectionObserver(loadImg, {
-//   root: null,
-//   threshold: 0,
-//   rootMargin: '200px',
-// });
-
-// imgTargets.forEach(img => imgObserver.observe(img));
-
-// ------------------------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------------------------
-////////////////// MODAL WINDOW ///////////////////--------------------------------------
-// ------------------------------------------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------------------------
-
-// const modal = document.querySelector('.modal');
-// const overlay = document.querySelector('.overlay');
-// const btnCloseModal = document.querySelector('.btn--close-modal');
-// const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
-// const openModal = function () {
-//   modal.classList.remove('hidden');
-//   overlay.classList.remove('hidden');
-// };
-// const closeModal = function (e) {
-//   e.preventDefault();
-//   modal.classList.add('hidden');
-//   overlay.classList.add('hidden');
-// };
-
-// btnsOpenModal.forEach(btn => {
-//   btn.addEventListener('click', openModal);
-// });
-
-// btnCloseModal.addEventListener('click', closeModal);
-// overlay.addEventListener('click', closeModal);
-
-// document.addEventListener('keydown', function (e) {
-//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-//     closeModal();
-//   }
-// });
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+btnNavEl.addEventListener("click", function () {
+  nav.classList.toggle("nav-open");
+});
 
 // ------------------------------------------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------------------------------------------
